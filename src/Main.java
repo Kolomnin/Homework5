3c61c54f9de6b736edc1ccd19c66a3c7ca9fe973
Kolomnin <kolomninsp@gmail.com>
2022-08-17 10:37:31 +0300

    первый коммит ДЗ№5 выполнены все задания * правка1 + исправление 6 задачи

--- src/Main.java
+++ src/Main.java
@@ -162,16 +162,11 @@ public class Main {
         System.out.println("\n" + "Задание 6");
         int[] array = {5,4,3,2,1};
         System.out.println(Arrays.toString(array));
-        int temp;
-        for (int i = 0; i < array.length-1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
+        int[] revers = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            revers[array.length - 1 -i] = array[i];
         }
+        array = revers;
         System.out.println(Arrays.toString(array));
         System.out.println("\n");
     }
